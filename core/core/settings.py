0747952ae36d6ffc0fe328a1d71df085e780f697
gabriel.stoffel <gabriel.stoffel@gmail.com>
2023-07-14 09:02:56 -0300

    .env

--- core/core/settings.py
+++ core/core/settings.py
@@ -12,16 +12,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import mongoengine
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env = environ.Env(DEBUG=(bool, False))
+# reading .env file
+env_file = os.path.join(BASE_DIR, ".env")
+environ.Env.read_env(env_file)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yyir_16fbk5buvpmc8wr3e$zcp@iy)5sgmp^lmrgk!j3x*5=pg'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
